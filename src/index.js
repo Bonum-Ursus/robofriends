01bc1cff35edae2cf3319ecd1ec03ead71fb3a97
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-01-17 14:51:24 +0500

    Delete useless files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello.js';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import {robots} from './robots'
 
-ReactDOM.render(<Hello greeting={"Hi, Man!"}/>, document.getElementById('root'));
+ReactDOM.render(
+        <div>
+            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        </div>
+        , document.getElementById('root'));
 
 serviceWorker.unregister();
