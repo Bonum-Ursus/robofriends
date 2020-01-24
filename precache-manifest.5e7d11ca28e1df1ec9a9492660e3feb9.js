21f1b58378b1cf04b3ef6a1c3c192f370215acdf
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-01-24 11:33:04 +0500

    Updates

---
+++ precache-manifest.5e7d11ca28e1df1ec9a9492660e3feb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d718a3fffef55f270fe81cf3c69793",
+    "url": "/Bonum-Ursus/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/Bonum-Ursus/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7d67d439e53678bec46d",
+    "url": "/Bonum-Ursus/robofriends/static/css/main.df12e036.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/Bonum-Ursus/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Bonum-Ursus/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d67d439e53678bec46d",
+    "url": "/Bonum-Ursus/robofriends/static/js/main.689602a0.chunk.js"
+  },
+  {
+    "revision": "51b6dd9bfe181005f56c",
+    "url": "/Bonum-Ursus/robofriends/static/js/runtime-main.4da8bea9.js"
+  },
+  {
+    "revision": "6a4ae4ece1f67f41c1a7d751c8e98802",
+    "url": "/Bonum-Ursus/robofriends/static/media/Ordeal-Eroded-FFP.6a4ae4ec.woff"
+  }
+]);
